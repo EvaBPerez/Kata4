2df0e836a089a6dee81f7741cc95a57b08e428de
Eva <Eva@DESKTOP-R21H1U5>
2020-11-18 14:04:34 +0000

    NEW HISTOGRAM, WITH PACKAGES

---
+++ src/kata4/Kata4.java
@@ -0,0 +1,25 @@
+package kata4;
+
+import kata4.view.MailHistogramBuilder;
+import kata4.persitence.MailListReader;
+import kata4.view.HistogramDisplay;
+import kata4.model.Histogram;
+import kata4.model.Mail;
+import java.io.IOException;
+import java.util.List;
+
+public class Kata4 {
+
+
+    public static void main(String[] args) throws IOException {
+        String fileName = new String ("email.txt");
+        List<Mail> mailList = MailListReader.read(fileName);
+        
+        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        
+        HistogramDisplay histogramDisplay = new HistogramDisplay("Histogram...", histogram);
+        histogramDisplay.execute();
+    }
+    
+    
+}
